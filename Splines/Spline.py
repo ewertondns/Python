f919ac49b6b1e747b252f58e827439c68d7383c0
Ewertondns <Ewertondnsantos@gmail.com>
2018-09-25 14:01:09 -0300

    Função Spline

---
+++ Splines/Spline.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+#Material de Consulta:
+#https://www.youtube.com/watch?v=Cm2mt7G_rgg
+'''
+https://pt.wikipedia.org/wiki/Spline
+http://www2.ic.uff.br/~aconci/splineatual.html
+http://www.cs.tau.ac.il/~turkel/notes/numeng/spline_note.pdf
+'''
+def splineLinear(function, xn):
+
